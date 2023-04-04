da690c8ff5c03527c12e35ca46fe0617b99d46fc
ZapeeoSheikh <muhammadrmz871@gmail.com>
2023-04-04 22:26:46 +0500

    display picture

--- lib/Pages/detailPage.dart
+++ lib/Pages/detailPage.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:love_calculator/Pages/mainScreen.dart';
+
 
 import '../Ui kit/ui.dart';
 
@@ -21,6 +21,7 @@ class DetailsPage extends StatefulWidget {
 }
 
 class _DetailsPageState extends State<DetailsPage> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -53,146 +54,154 @@ class _DetailsPageState extends State<DetailsPage> {
               SizedBox(
                 height: 10,
               ),
-              RichText(
-                  text: TextSpan(
-                      style: TextStyle(fontSize: 25, letterSpacing: 0.5),
-                      children: [
-                    TextSpan(
-                      text: "Your ",
-                      style: TextStyle(
-                          color: MyColor.mainColor1,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    TextSpan(
-                      text: "Summary",
-                      style: TextStyle(
-                        color: MyColor.textColor1,
-                      ),
-                    ),
-                  ])),
-              SizedBox(
-                height: 50,
-              ),
-              Center(
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 1,
-                        blurRadius: 5,
-                        offset: Offset(0, 3), // changes position of shadow
-                      ),
-                    ],
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(50.0),
-                    child: Column(
-                      children: [
-                        RichText(
-                            text: TextSpan(
-                                style: TextStyle(
-                                  fontSize: 35,
-                                ),
-                                children: [
-                              TextSpan(
-                                text: "Your Love is",
-                                style: TextStyle(
-                                  color: MyColor.textColor1,
-                                ),
+
+              Column(
+                children: [
+                  RichText(
+                      text: TextSpan(
+                          style: TextStyle(fontSize: 25, letterSpacing: 0.5),
+                          children: [
+                            TextSpan(
+                              text: "Your ",
+                              style: TextStyle(
+                                  color: MyColor.mainColor1,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            TextSpan(
+                              text: "Summary",
+                              style: TextStyle(
+                                color: MyColor.textColor1,
                               ),
-                            ])),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        RichText(
-                            text: TextSpan(
-                                style:
+                            ),
+                          ])),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  Center(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 1,
+                            blurRadius: 5,
+                            offset: Offset(0, 3), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(50.0),
+                        child: Column(
+                          children: [
+                            RichText(
+                                text: TextSpan(
+                                    style: TextStyle(
+                                      fontSize: 35,
+                                    ),
+                                    children: [
+                                      TextSpan(
+                                        text: "Your Love is",
+                                        style: TextStyle(
+                                          color: MyColor.textColor1,
+                                        ),
+                                      ),
+                                    ])),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            RichText(
+                                text: TextSpan(
+                                    style:
                                     TextStyle(fontSize: 65, letterSpacing: 0.5),
-                                children: [
-                              TextSpan(
-                                text: "${widget.percentage}%",
-                                style: TextStyle(
-                                    color: MyColor.mainColor1,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ])),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 50,
-              ),
-              RichText(
-                  text: TextSpan(
-                      style: TextStyle(fontSize: 22, letterSpacing: 0.5),
-                      children: [
-                    TextSpan(
-                      text: "${widget.maleName}",
-                      style: TextStyle(
-                          color: MyColor.mainColor1,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    TextSpan(
-                      text: " and ",
-                      style: TextStyle(
-                        color: MyColor.textColor1,
+                                    children: [
+                                      TextSpan(
+                                        text: "${widget.percentage}%",
+                                        style: TextStyle(
+                                            color: MyColor.mainColor1,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ])),
+                          ],
+                        ),
                       ),
                     ),
-                    TextSpan(
-                      text: "${widget.femaleName} ",
-                      style: TextStyle(
-                          color: MyColor.mainColor1,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ])),
-              SizedBox(
-                height: 70,
-              ),
-              Text(
-                "Result : ",
-                style: TextStyle(
-                    color: MyColor.textColor1,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  color: MyColor.mainColor1,
-                  borderRadius: BorderRadius.circular(10),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 1,
-                      blurRadius: 5,
-                      offset: Offset(0, 3), // changes position of shadow
-                    ),
-                  ],
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: RichText(
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  RichText(
                       text: TextSpan(
                           style: TextStyle(fontSize: 22, letterSpacing: 0.5),
                           children: [
-                        TextSpan(
-                          text: "${widget.result}",
-                          style: TextStyle(
-                              color: Colors.white, fontWeight: FontWeight.bold),
+                            TextSpan(
+                              text: "${widget.maleName}",
+                              style: TextStyle(
+                                  color: MyColor.mainColor1,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            TextSpan(
+                              text: " and ",
+                              style: TextStyle(
+                                color: MyColor.textColor1,
+                              ),
+                            ),
+                            TextSpan(
+                              text: "${widget.femaleName} ",
+                              style: TextStyle(
+                                  color: MyColor.mainColor1,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ])),
+                  SizedBox(
+                    height: 70,
+                  ),
+                  Text(
+                    "Result : ",
+                    style: TextStyle(
+                        color: MyColor.textColor1,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: MyColor.mainColor1,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 1,
+                          blurRadius: 5,
+                          offset: Offset(0, 3), // changes position of shadow
                         ),
-                      ])),
-                ),
+                      ],
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: RichText(
+                          text: TextSpan(
+                              style: TextStyle(fontSize: 22, letterSpacing: 0.5),
+                              children: [
+                                TextSpan(
+                                  text: "${widget.result}",
+                                  style: TextStyle(
+                                      color: Colors.white, fontWeight: FontWeight.bold),
+                                ),
+                              ])),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                ],
               ),
-              SizedBox(
-                height: 50,
-              ),
+
+
+
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
